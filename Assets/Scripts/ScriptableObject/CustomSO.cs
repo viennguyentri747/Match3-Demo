6cffc8517d1d2b40149fca8816aecdc89dabae0c
Vien Nguyen <viennguyentri747@gmail.com>
2023-10-21 21:57:25 -0500

    Add ScriptableObject to store PrizeData, Create 4 main SOs

---
+++ Assets/Scripts/ScriptableObject/CustomSO.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Match3Bonus
+{
+    public class CustomSO : ScriptableObject
+    {
+        [SerializeField] private string _description;
+    }
+}
---
+++ Assets/Scripts/ScriptableObject/SOPrize.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+namespace Match3Bonus
+{
+    [CreateAssetMenu(fileName = nameof(SOPrize), menuName = "Data/" + nameof(SOPrize))]
+    public class SOPrize : CustomSO
+    {
+        [SerializeField] private string _name;
+        [SerializeField] private int _reward;
+        [SerializeField] private int _totalCount;
+        [SerializeField] private TokenAsset _tokenAsset;
+
+        public string Name => _name;
+        public int Reward => _reward;
+        public int TotalCount => _totalCount;
+        public TokenAsset TokenAsset => _tokenAsset;
+    }
+
+    [Serializable]
+    public class TokenAsset
+    {
+        [SerializeField] private AnimatorOverrideController _animOverrideController;
+    }
+}
