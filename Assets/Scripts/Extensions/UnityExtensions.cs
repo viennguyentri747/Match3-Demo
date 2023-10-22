450b09ab7da2c956c95b1948f9e808fd85df9258
Vien Nguyen <viennguyentri747@gmail.com>
2023-10-21 19:57:01 -0500

    Add necessary extensions

---
+++ Assets/Scripts/Extensions/UnityExtensions.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Match3Bonus
+{
+    public static class UnityExtensions
+    {
+        public static void SetActive(this MonoBehaviour mono, bool isActive)
+        {
+            mono.gameObject.SetActive(isActive);
+        }
+
+        public static void ShowCachedViews<TView, TData>(this TView viewTemplate, IList<TData> viewDatas,
+            IList<TView> cacheViews, Action<TView, TData> setupView) where TView : MonoBehaviour
+        {
+            {
+                viewTemplate.SetActive(false);
+                for (int i = 0; i < viewDatas.Count; i++)
+                {
+                    TView view;
+                    if (i < cacheViews.Count)
+                    {
+                        view = cacheViews[i];
+                    }
+                    else
+                    {
+                        view = UnityEngine.Object.Instantiate(viewTemplate, viewTemplate.transform.parent);
+                        cacheViews.Add(view);
+                    }
+
+                    view.SetActive(true);
+                    setupView?.Invoke(view, viewDatas[i]);
+                }
+
+                for (int i = viewDatas.Count; i < cacheViews.Count; i++)
+                {
+                    cacheViews[i].SetActive(false);
+                }
+            }
+        }
+    }
+}
