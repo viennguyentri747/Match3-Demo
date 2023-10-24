337fae1a905f7a595c6c794f783d8bb4ece152ea
Vien Nguyen <viennguyentri747@gmail.com>
2023-10-24 16:55:08 -0500

    Update ShowViews extensions for showing specified numer of view

--- Assets/Scripts/Common/Extensions/UnityExtensions.cs
+++ Assets/Scripts/Common/Extensions/UnityExtensions.cs
@@ -7,45 +7,47 @@ namespace Match3Bonus
 {
     public static class UnityExtensions
     {
-        private static void SetActive(this MonoBehaviour mono, bool isActive)
-        {
-            mono.gameObject.SetActive(isActive);
-        }
-
-        public static void ShowCachedViews<TView, TData>(this TView viewTemplate, IEnumerable<TData> viewDatas,
-            IList<TView> cacheViews, Action<TView, TData> setupView) where TView : MonoBehaviour
-        {
-            viewTemplate.ShowCachedViews(viewDatas as List<TData> ?? viewDatas.ToList(), cacheViews, setupView);
-        }
-
         public static void ShowCachedViews<TView, TData>(this TView viewTemplate, IList<TData> viewDatas,
             IList<TView> cacheViews, Action<TView, TData> setupView) where TView : MonoBehaviour
         {
             {
-                viewTemplate.SetActive(false);
-                int datasCount = viewDatas.Count;
-                for (int i = 0; i < datasCount; i++)
+                ShowCachedViews(viewTemplate, viewDatas.Count, cacheViews, (view, index) =>
                 {
-                    TView view;
-                    if (i < cacheViews.Count)
-                    {
-                        view = cacheViews[i];
-                    }
-                    else
-                    {
-                        view = UnityEngine.Object.Instantiate(viewTemplate, viewTemplate.transform.parent);
-                        cacheViews.Add(view);
-                    }
-
-                    view.SetActive(true);
-                    setupView?.Invoke(view, viewDatas[i]);
-                }
-
-                for (int i = datasCount; i < cacheViews.Count; i++)
-                {
-                    cacheViews[i].SetActive(false);
-                }
+                    setupView?.Invoke(view, viewDatas[index]);
+                });
             }
+        }
+
+        public static void ShowCachedViews<TView>(this TView viewTemplate, int total,
+            IList<TView> cacheViews, Action<TView, int> setupAtIndex) where TView : MonoBehaviour
+        {
+            viewTemplate.SetActive(false);
+            for (int i = 0; i < total; i++)
+            {
+                TView view;
+                if (i < cacheViews.Count)
+                {
+                    view = cacheViews[i];
+                }
+                else
+                {
+                    view = UnityEngine.Object.Instantiate(viewTemplate, viewTemplate.transform.parent);
+                    cacheViews.Add(view);
+                }
+
+                view.SetActive(true);
+                setupAtIndex?.Invoke(view, i);
+            }
+
+            for (int i = total; i < cacheViews.Count; i++)
+            {
+                cacheViews[i].SetActive(false);
+            }
+        }
+
+        private static void SetActive(this MonoBehaviour mono, bool isActive)
+        {
+            mono.gameObject.SetActive(isActive);
         }
     }
 }
