d0fde63859ec36a12e5221a2c141aee2a17cc6f6
Vien Nguyen <viennguyentri747@gmail.com>
2023-10-25 20:12:06 -0500

    Add canvas revert fade

--- Assets/Scripts/Common/UI/CanvasFader.cs
+++ Assets/Scripts/Common/UI/CanvasFader.cs
@@ -11,22 +11,55 @@ namespace Match3Bonus
         [Range(0, 1f)] [SerializeField] private float _endAlpha;
         [SerializeField] private float _fadeDuration;
         [SerializeField] private UnityEvent _onFadeComplete;
+        [SerializeField] private UnityEvent _onRevertFadeComplete;
 
+        private Coroutine _fadeCoroutine;
         private readonly CountDownRoutine _countDownRoutine = new();
+        private bool _isReverting;
 
         public void StartFade()
         {
-            StartCoroutine(UpdateFade());
+            StopCoroutineFade();
+            _fadeCoroutine = StartCoroutine(UpdateFade(_startAlpha, _endAlpha));
+            _isReverting = false;
         }
 
-        private IEnumerator UpdateFade()
+        public void StartRevertFade()
         {
-            yield return _countDownRoutine.RoutineCountdownInvoke(_fadeDuration, () => _onFadeComplete?.Invoke(),
+            StopCoroutineFade();
+            _fadeCoroutine = StartCoroutine(UpdateFade(_endAlpha, _startAlpha));
+            _isReverting = true;
+        }
+
+        private void StopCoroutineFade()
+        {
+            if (_fadeCoroutine != null)
+            {
+                StopCoroutine(_fadeCoroutine);
+            }
+        }
+
+        private IEnumerator UpdateFade(float startAlpha, float endAlpha)
+        {
+            yield return _countDownRoutine.RoutineCountdownInvoke(_fadeDuration,
+                () =>
+                {
+                    if (_isReverting)
+                    {
+                        _onRevertFadeComplete?.Invoke();
+                    }
+                    else
+                    {
+                        _onFadeComplete?.Invoke();
+                    }
+                },
                 (timeCountDown) =>
                 {
                     float progress = (_fadeDuration - timeCountDown)/ _fadeDuration;
-                    _canvasGroup.alpha = Mathf.Lerp(_startAlpha, _endAlpha, progress);
+                    _canvasGroup.alpha = Mathf.Lerp(startAlpha, endAlpha, progress);
                 });
+
+
         }
     }
 }
